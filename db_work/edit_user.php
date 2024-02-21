997590cc8525af5a474e2ae5dab8a879fcb999b5
ranju58 <158138118+ranju58@users.noreply.github.com>
2024-02-21 18:22:44 +0545

    Update edit_user.php

--- db_work/edit_user.php
+++ db_work/edit_user.php
@@ -1,53 +1,55 @@
 <?php
 include("connection.php");
-if( isset($_GET['edit']) )  
+if( isset($_GET['update']) )  
 {  
 $id = $_GET['ID'];  
-$res= mysql_query("SELECT * FROM team_members WHERE ID='$id'");  
-$row= mysql_fetch_array($res);  
+$query=mysqli_query($conn,"select * from `team_members` where ID='$id'");
+$row=mysqli_fetch_array($query);
 }  
    
-if( isset($_POST['ID']) )  
-{  
- $id   = $_POST['ID'];
+if( isset($_POST['update']) )  
+{ 
+ $id   = $_GET['ID'];
 $fullname = $_POST['fullname'];
 $password = $_POST['password']; 
-$address = $_POST['Address'];
-$email = $_POST['Email'];
-$phone = $_POST['Phone'];      
+$address = $_POST['address'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];      
 
+$sql  = "UPDATE team_members SET fullname='$fullname', password='$password', address='$address', email='$email', phone='$phone' WHERE ID='$id'";  
+echo '<br>'.$sql;
 
-$sql  = "UPDATE team_members SET fullname='$fullname', password='$password',address='$address',email='$email',phone='$phone' WHERE id='$id'";  
-$res = mysql_query($sql) 
-    or die("Could not update".mysql_error());  
+$res = mysqli_query($conn,$sql) 
+    or die("Could not update".mysqli_error());  
+    header('location:view_users.php');
 }  
-   
-?>  
+?>
 
 <!doctype html>
 <html lang="en-US">
-  <head>
+
+<head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width" />
-    <title>Database</title>  
-  </head>
-  <body>
+    <title>Database</title>
+</head>
 
-    <form action="edit_user.php" method="POST">
-        <label for ="User_ID">ID</label>
-        <input type="text" name="user_id" id="user_id" placeholder="user_id" ><br><br>
-        <label for ="Fullname">Fullname</label>
-        <input type="text" name="fullname" id="fullname" placeholder="fullname" ><br><br>
-        <label for ="Password">Password</label>
-        <input type="password" name="password" id="password" placeholder="password" ><br><br>
-        <label for ="Address">Address</label>
-        <input type="text" name="address" id="address" placeholder="address" ><br><br>
-        <label for ="Email">Email</label>
-        <input type="email" name="email" id="email" placeholder="Email" ><br><br>
-        <label for ="Phone">Phone</label>
-        <input type="text" name="phone" id="phone" placeholder="Phone" ><br><br>
-        <button type ="Submit" name ="update">Update User</button><br>
-</form>
+<body>
+    <form method="POST" action="">
+        <!-- <label for="User_ID">ID</label> -->
+        <!-- <input type="text" name="user_id" placeholder="user_id"><br><br> -->
+        <label for="Fullname">Fullname</label>
+        <input type="text" name="fullname" id="fullname" placeholder="fullname"><br><br>
+        <label for="Password">Password</label>
+        <input type="password" name="password" id="password" placeholder="password"><br><br>
+        <label for="Address">Address</label>
+        <input type="text" name="address" id="address" placeholder="address"><br><br>
+        <label for="Email">Email</label>
+        <input type="email" name="email" id="email" placeholder="Email"><br><br>
+        <label for="Phone">Phone</label>
+        <input type="number" name="phone" id="phone" placeholder="Phone"><br><br>
+        <button type="Submit" name="update">Update User</button><br>
+    </form>
 </body>
-</html>
 
+</html>
